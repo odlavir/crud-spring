a0bb0fdf40afd37835d9b5a3768972cff6b82d2b
Odlavir Florentino <lavirandrade@gmail.com>
2023-02-28 13:45:18 -0300

    Implementando o soft delete na API.

--- src/main/java/com/lavir/crudspring/model/Course.java
+++ src/main/java/com/lavir/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.lavir.crudspring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,6 +18,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?;")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
   @Id
@@ -35,4 +39,9 @@ public class Course {
   @Column(length = 10, nullable = false)
   private String category;
 
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Ativo|Inativo")
+  @Column(length = 10, nullable = false)
+  private String status = "Ativo";
 }
